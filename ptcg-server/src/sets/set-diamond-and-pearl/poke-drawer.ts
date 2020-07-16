3c012028533a780df28913924af1ee15a16ef7a8
keeshii <keeshii@ptcg.eu>
2020-07-16 15:29:44 +0200

    [server] poke-drawer, poke-blower.

--- ptcg-server/src/sets/set-diamond-and-pearl/poke-drawer.ts
+++ ptcg-server/src/sets/set-diamond-and-pearl/poke-drawer.ts
@@ -1,27 +1,94 @@
+import { Card } from "../../game/store/card/card";
 import { TrainerCard } from "../../game/store/card/trainer-card";
 import { TrainerType } from "../../game/store/card/card-types";
 import { StoreLike } from "../../game/store/store-like";
 import { State } from "../../game/store/state/state";
 import { Effect } from "../../game/store/effects/effect";
+import { PlayTrainerEffect } from "../../game/store/effects/play-card-effects";
+import { ConfirmPrompt } from "../../game/store/prompts/confirm-prompt";
+import { CardMessage } from "../card-message";
+import { ChooseCardsPrompt } from "../../game/store/prompts/choose-cards-prompt";
+import { ShuffleDeckPrompt } from "../../game/store/prompts/shuffle-prompt";
 
-export class PokeBlower extends TrainerCard {
+function* playCard(next: Function, store: StoreLike, state: State, effect: PlayTrainerEffect): IterableIterator<State> {
+  const player = effect.player;
+  const name = effect.trainerCard.name;
+
+  const count = player.hand.cards.reduce((sum, c) => {
+    return sum + (c.name === name ? 1 : 0);
+  }, 0);
+  let playTwoCards = false;
+
+  if (count >= 2) {
+    yield store.prompt(state, new ConfirmPrompt(
+      player.id,
+      CardMessage.PLAY_BOTH_CARDS_AT_ONCE
+    ), result => {
+      playTwoCards = result;
+      next();
+    });
+  }
+
+  if (playTwoCards === false) {
+    player.deck.moveTo(player.hand, 1);
+    return state;
+  }
+
+  // Discard second Poke-Drawer +
+  const second = player.hand.cards.find(c => {
+    return c.name === name && c !== effect.trainerCard
+  });
+  if (second !== undefined) {
+    player.hand.moveCardTo(second, player.discard);
+  }
+
+  let cards: Card[] = [];
+  yield store.prompt(state, new ChooseCardsPrompt(
+    player.id,
+    CardMessage.CHOOSE_ANY_TWO_CARDS,
+    player.deck,
+    { },
+    { min: 0, max: 2, allowCancel: true }
+  ), selected => {
+    cards = selected;
+    next();
+  });
+
+  // Get selected cards
+  player.deck.moveCardsTo(cards, player.hand);
+
+  // Shuffle the deck
+  yield store.prompt(state, new ShuffleDeckPrompt(player.id), order => {
+    player.deck.applyOrder(order);
+    next();
+  });
+
+  return state;
+}
+
+export class PokeDrawer extends TrainerCard {
 
   public trainerType: TrainerType = TrainerType.ITEM;
 
   public set: string = 'DP';
 
-  public name: string = 'Poke Blower +';
+  public name: string = 'Poke Drawer +';
 
-  public fullName: string = 'Poke Blower SF';
+  public fullName: string = 'Poke Drawer SF';
 
   public text: string =
-    'You may play 2 Poke Blower + at the same time. If you play 1 ' +
-    'Poke Blower +, flip a coin. If heads, put 1 damage counter on 1 of your ' +
-    'opponent\'s Pokemon. If you play 2 Poke Blower +, choose 1 of your ' +
-    'opponent\'s Benched Pokemon and switch it with 1 of your opponent\'s ' +
-    'Active Pokemon.';
+    'You may play 2 Poke Drawer + at the same time. If you play 1 ' +
+    'Poke Drawer +, draw a card. If you play 2 Poke Drawer +, search your ' +
+    'deck for up to 2 cards, and put them into your hand. Shuffle your deck ' +
+    'afterward.';
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
+    if (effect instanceof PlayTrainerEffect && effect.trainerCard === this) {
+      let generator: IterableIterator<State>;
+      generator = playCard(() => generator.next(), store, state, effect);
+      return generator.next().value;
+    }
+
     return state;
   }
 
