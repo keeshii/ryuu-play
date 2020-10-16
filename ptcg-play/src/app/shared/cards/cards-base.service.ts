d857dd6594986aecdb00e8690bc1b40ca38f0924
keeshii <keeshii@ptcg.eu>
2020-10-16 11:38:21 +0200

    [ptcg] sort cards.

--- ptcg-play/src/app/shared/cards/cards-base.service.ts
+++ ptcg-play/src/app/shared/cards/cards-base.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Card, StateSerializer } from 'ptcg-server';
+import { Card, StateSerializer, SuperType, PokemonCard, EnergyCard, CardType, TrainerCard } from 'ptcg-server';
 
 import { ApiService } from '../../api/api.service';
 import { CardInfoPopupData, CardInfoPopupComponent } from './card-info-popup/card-info-popup.component';
@@ -7,7 +7,6 @@ import { CardInfoListPopupComponent } from './card-info-list-popup/card-info-lis
 import { CardInfoPaneAction } from './card-info-pane/card-info-pane.component';
 import { MatDialog } from '@angular/material/dialog';
 import { SessionService } from '../session/session.service';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +25,42 @@ export class CardsBaseService {
   public setCards(cards: Card[]) {
     this.cards = cards;
     this.names = this.cards.map(c => c.fullName);
+    this.cards.sort(this.compareCards);
     StateSerializer.setKnownCards(this.cards);
+  }
+
+  private compareCards(c1: Card, c2: Card) {
+    if (c1.superType !== c2.superType) {
+      return c1.superType - c2.superType;
+    }
+    switch (c1.superType) {
+      case SuperType.POKEMON:
+        const p1 = c1 as PokemonCard;
+        const p2 = c2 as PokemonCard;
+        if (p2.cardType !== p1.cardType) {
+          return p1.cardType - p2.cardType;
+        }
+        break;
+      case SuperType.ENERGY:
+        const e1 = c1 as EnergyCard;
+        const e2 = c2 as EnergyCard;
+        if (e1.energyType !== e2.energyType) {
+          return e1.energyType - e2.energyType;
+        }
+        const type1 = e1.provides.length > 0 ? e1.provides[0] : CardType.NONE;
+        const type2 = e2.provides.length > 0 ? e2.provides[0] : CardType.NONE;
+        if (type1 !== type2) {
+          return type1 - type2;
+        }
+        break;
+      case SuperType.TRAINER:
+        const t1 = c1 as TrainerCard;
+        const t2 = c2 as TrainerCard;
+        if (t1.trainerType !== t2.trainerType) {
+          return t1.trainerType - t2.trainerType;
+        }
+    }
+    return c1.fullName < c2.fullName ? -1 : 1;
   }
 
   public getCards(): Card[] {
