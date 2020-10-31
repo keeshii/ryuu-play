a0d607fc7259baea1ca5d5e4bc823dd1eeaff50a
keeshii <keeshii@ptcg.eu>
2020-10-31 13:52:54 +0100

    [ptcg] translate ui.

--- ptcg-play/src/app/app.module.ts
+++ ptcg-play/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { SkyhookDndModule } from '@angular-skyhook/core';
 import { MultiBackend, HTML5ToTouch } from '@angular-skyhook/multi-backend';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 import { ApiModule } from './api/api.module';
 import { AppComponent } from './app.component';
@@ -34,9 +38,34 @@ import { TableModule } from './table/table.module';
     ReplaysModule,
     SharedModule,
     SkyhookDndModule.forRoot({ backend: MultiBackend, options: HTML5ToTouch }),
-    TableModule
+    TableModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: ( createTranslateLoader ),
+        deps: [ HttpClient ]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+
+    let browserLang = translate.getBrowserLang();
+
+    // decode borser language, strip format 'en-US' to 'en'
+    if (browserLang !== undefined && browserLang.match(/\w\w/i) !== null) {
+      browserLang = browserLang.match(/\w\w/i)[0].toLowerCase();
+      translate.use(browserLang);
+    }
+  }
+}
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
